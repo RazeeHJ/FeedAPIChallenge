8c473ee1f6f8404288082430c98685d31534be07
Razee <razee.hj@gmail.com>
2023-10-27 18:27:30 +0200

    Remove `URLRequest` from loader class

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let urlRequest: URLRequest
+	private let url: URL
 	private let client: HTTPClient
 
 	public enum Error: Swift.Error {
@@ -13,13 +13,15 @@ public final class RemoteFeedLoader: FeedLoader {
 		case invalidData
 	}
 
-	public init(urlRequest: URLRequest, client: HTTPClient) {
-		self.urlRequest = urlRequest
+	public init(url: URL, client: HTTPClient) {
+		self.url = url
 		self.client = client
 	}
 
 	public func load() async throws -> [FeedImage] {
-		let (data, response) = try await self.client.perform(from: self.urlRequest)
+		let urlRequest = URLRequest(url: url)
+
+		let (data, response) = try await self.client.perform(from: urlRequest)
 		return try FeedImageMapper.map(data, response: response)
 	}
 }
